26c646611c33dd772d624042b37376ee2c2262ef
aitorlarrinoa <aitorlarrinoa231@gmail.com>
2022-01-09 10:00:55 +0100

    Two ways of doing model deployment

--- big_practice/Flask_projects/model_deployment/app.py
+++ big_practice/Flask_projects/model_deployment/app.py
@@ -53,6 +53,24 @@ def predict():
             NA_sin_C_WDAY = np.sin(2*math.pi*wday/7)
             data_dictionary["NA_cos_C_WDAY"] = NA_cos_C_WDAY
             data_dictionary["NA_sin_C_WDAY"] = NA_sin_C_WDAY
+        elif variable == "num_C_YEAR":
+            value_c_year = int(request.form.get(variable))
+            mean_c_year_test = 2006.0155218348198
+            sdv_c_year_test = 4.567695934222996
+            new_value_year = (value_c_year - mean_c_year_test)/sdv_c_year_test
+            data_dictionary["num_C_YEAR"] = new_value_year
+        elif variable == "num_Random":
+            value_Random = int(request.form.get(variable))
+            mean_Random_test = 49.51538088565281
+            sdv_Random_test = 28.850311918400497
+            new_value_Random = (value_Random-mean_Random_test)/sdv_Random_test
+            data_dictionary["num_Random"] = new_value_Random
+        elif variable == "num_C_VEHS":
+            value_c_vehs = int(request.form.get(variable))
+            mean_c_vehs_test = 2.0971652406757
+            sdv_c_vehs_test = 1.3324953572064602
+            new_value_vehs = (value_c_vehs-mean_c_vehs_test)/sdv_c_vehs_test
+            data_dictionary["num_C_VEHS"] = new_value_vehs
         else:
             data_dictionary[variable] = request.form.get(variable)
     query_df = pd.DataFrame(data_dictionary, index = [0])
